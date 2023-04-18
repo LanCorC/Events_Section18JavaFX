3d86bede91d1fe6120bdf28337347f971520f418
Lance <lanc3.corcega@gmail.com>
2023-04-18 17:04:29 +1200

    Section 18.251 (JavaFX; Events and Event Handlers)

--- src/main/java/com/example/events_section18javafx/HelloController.java
+++ src/main/java/com/example/events_section18javafx/HelloController.java
@@ -2,13 +2,14 @@ package com.example.events_section18javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField nameField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onButtonClicked() {
+        System.out.println("Hello, " + nameField.getText());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.events_section18javafx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.events_section18javafx to javafx.fxml;
     exports com.example.events_section18javafx;
 }
